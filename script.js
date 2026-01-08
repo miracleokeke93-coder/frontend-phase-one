61ca7a5ffa2489ee86c5f89c30e2d72d5ed188a7
Miracle Okeke <miracleokeke93@gmail.com>
2026-01-08 19:25:39 +0100

    Refractor HTML, CSS, and JS for clarity and accessibilty

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menuButton = document.querySelector(".menu-toggle");
 const navList = document.querySelector(".nav-list");
 
+if (menuButton && navList) {
 menuButton.addEventListener("click, () => {
     navList.classList.toggle("show");
-});
+}};
+}
